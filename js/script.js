5e413222f7cd429df1d5b4351af8045021b77443
Guadalupe Camacho <gcamacho079@gmail.com>
2017-05-05 15:10:59 -0700

    Remove bootstrap, brain img, and start script from scratch

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-$(document).ready(function() {
+var wikiViewer = {
+  onReady: function() {
+    $("#searchQuery").keydown(wikiViewer.checkKey); // Snags query when enter is pressed
+    $("#submitButton").click(wikiViewer.getQuery); // Snags query when input button is pushed
+  },
 
-  $("#search").on("submit", function(event) {
-    event.preventDefault();
-    var searchFor = $("#search").serialize();
-    searchFor = searchFor.substring(12, searchFor.length);
-    console.log(searchFor);
-  })
+  checkKey: function() {
+    if (event.which == 13) {
+      console.log($("#searchQuery").val());
+    }
+  },
 
+  getQuery: function() {
+    console.log($("#searchQuery").val());
+  }
+}
 
-})
+$(document).ready(wikiViewer.onReady);
