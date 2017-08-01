8463fbb0389d1708ad0661bac25a288276fb224d
Guadalupe Camacho <gcamacho079@gmail.com>
2017-08-01 08:40:28 -0700

    Update javascript

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
 var wikiViewer = {
   onReady: function() {
     $("#searchQuery").keydown(wikiViewer.checkKey); // Snags query when enter is pressed
-    $("#submitButton").click(wikiViewer.getQuery); // Snags query when input button is pushed
+    $("#submitButton").click(wikiViewer.fetchAndMake); // Snags query when input button is pushed
   },
 
   checkKey: function(event) {
     if (event.which == 13) {
       event.preventDefault(); // Prevents page reload
-      wikiViewer.getQuery();
+      wikiViewer.fetchAndMake();
     }
   },
 
-  getQuery: function() {
-    wikiViewer.buildUrl($("#searchQuery").val())
+  fetchAndMake: function() {
+    var link = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + $("#searchQuery").val() + "&format=json&origin=*";
+    wikiViewer.callAPI(link);
   },
 
-  buildUrl: function(value) {
-    var link = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + value + "&format=json";
-    console.log(link);
+  callAPI: function(wikiLink) {
+    $.ajax(wikiLink, {
+      dataType: "json",
+      type: "GET",
+      success: function(data) {
+        //console.log(data);
+        $.each(data[1], function(index, value) {
+          console.log(value + data[2][index] + data[3][index]); // Logs Wiki info using index from first array
+
+        });
+      }
+
+    }); // End wiki json call*/
+  },
+
+  entryInfo: function(name, url, article) {
+    //console.log(name + url + article);
   }
 }
 
